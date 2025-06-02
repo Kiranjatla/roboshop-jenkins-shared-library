dd9a491aabdbfaca5b97e6e4fbcb988d6567869d
KIRAN <ks@gmail.com>
2025-06-02 22:49:54 +0530

    libraray file

---
+++ vars/python.groovy
@@ -0,0 +1,51 @@
+def call(){
+    pipeline{
+
+        agent any
+
+        stages{
+            stage('Code Quality'){
+                steps{
+                    echo 'code quality'
+                    sh 'env'
+                }
+            }
+
+            stage('Style Checks'){
+                when{
+                    anyOf{
+                        branch 'main'
+                        tag "*"
+                    }
+                }
+                steps{
+                    echo 'Style Checks'
+                }
+            }
+            stage('Unit Tests'){
+                when{
+                    anyOf{
+                        branch 'main'
+                        tag "*"
+                    }
+                }
+                steps{
+                    echo 'Unit Tests'
+                }
+            }
+            stage('Prepare Artifact'){
+                when{ tag "*" }
+                steps{
+                    echo 'Prepare Artifact'
+                }
+            }
+            stage('Publish Artifact'){
+                when{ tag "*" }
+                steps{
+                    echo 'Publish Artifact'
+                }
+            }
+        }
+
+    }
+}
