0fbd9d3de1a4df4520e19625e263b45bd2f51fef
KIRAN <ks@gmail.com>
2025-06-03 19:49:46 +0530

    libraray file

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,7 +4,7 @@ def codeQuality(){
         }
     }
 def codeChecks(){
-if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
+    if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
     stage('Style Checks') {
         echo 'Style Checks'
     }
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,34 +4,9 @@ def call() {
         if (! env.TAG_NAME) {
             env.TAG_NAME = ""
         }
-        sh 'env'
         common.codeQuality()
         common.codeChecks()
         common.artifacts()
 
-//            stage('Unit Tests'){
-//                when {
-//                    anyOf {
-//                        branch 'main'
-//                        tag "*"
-//                    }
-//                }
-//                steps {
-//                    echo 'Unit Tests'
-//                }
-//            }
-//            stage('Prepare Artifact'){
-//                when { tag "*" }
-//                steps {
-//                    echo 'Prepare Artifact'
-//                }
-//            }
-//            stage('Publish Artifact'){
-//                when { tag "*" }
-//                steps {
-//                    echo 'Publish Artifact'
-//                }
-//            }
-//        }
     }
 }
