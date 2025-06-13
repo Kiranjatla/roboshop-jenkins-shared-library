e15c2eeffc900ab72f47e78d5b07b41916eebc9b
KIRAN <ks@gmail.com>
2025-06-13 20:36:55 +0530

    env

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,7 +3,7 @@ def codeQuality(){
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
 
             sh '''
-        sonar-scanner -Dsonar.host.url=http://172.31.82.144:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectkey=${COMPONENT}
+        sonar-scanner -Dsonar.host.url=http://172.31.82.144:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT}
         '''
         }
     }
