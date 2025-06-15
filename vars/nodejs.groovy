5b8a4132c608990664c88f5beb1369737139c1e0
KIRAN <ks@gmail.com>
2025-06-15 11:37:02 +0530

    quality gate

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,8 +5,7 @@ def call() {
         common.codeChecks()
         common.artifacts()
 
-    }
-        catch (Exception){
+    } catch (Exception e){
             mail bcc: '', body: 'Build Failure ${RUN_DISPLAY_URL}', cc: '', from: 'kiranjatla9@gmail.com', replyTo: '', subject: 'Build failed', to: 'kiranjatla9@gmail.com'
         }
     }
