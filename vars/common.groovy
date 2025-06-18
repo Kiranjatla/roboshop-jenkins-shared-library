375f0fa2df7924c08c74a7ff82c371b6142c341e
KIRAN <ks@gmail.com>
2025-06-18 19:31:13 +0530

    quality gate

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,9 @@
+def codeCheckout() {
+    stage('Code Checkout') {
+        sh 'find . | sed 1d |xargs rm -rf'
+        git branch: 'main', url: "https://https://github.com/Kiranjatla/${COMPONENT}.git"
+    }
+}
 def codeQuality(){
     stage('Code Quality') {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,10 @@
 def call() {
     node {
         try {
-        common.codeQuality()
-        common.codeChecks()
-        common.artifacts()
+            common.codeCheckout()
+            common.codeQuality()
+            common.codeChecks()
+            common.artifacts()
 
     } catch (Exception e){
             mail bcc: '', body: 'Build Failure ${RUN_DISPLAY_URL}', cc: '', from: 'kiranjatla9@gmail.com', replyTo: '', subject: 'Build failed', to: 'kiranjatla9@gmail.com'
