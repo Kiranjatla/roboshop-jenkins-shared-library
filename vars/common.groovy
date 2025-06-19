610c0771a79c61b15b376a9a60abab2cd028c456
KIRAN <ks@gmail.com>
2025-06-19 18:54:39 +0530

    quality gate

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,7 +8,7 @@ def codeQuality(){
     stage('Code Quality') {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
             sh '''
-        #sonar-scanner -Dsonar.host.url=http://172.31.82.144:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}
+        #sonar-scanner -Dsonar.host.url=http://172.31.39.42:9000 -Dsonar.login=${sonarUser} -Dsonar.password=${sonarPass} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}
         echo ok
         '''
         }
