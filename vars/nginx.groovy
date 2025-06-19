3b5bb2a7c8a27c34a14b37b9139751597677af71
KIRAN <ks@gmail.com>
2025-06-19 22:56:30 +0530

    quality gate

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -1,12 +1,11 @@
 def call() {
-    env.APPTYPE == "nginx"
+    env.APPTYPE = "nginx"
     node {
         try {
             common.codeCheckout()
             common.codeQuality()
             common.codeChecks()
             common.artifacts()
-
         } catch (Exception e){
             mail bcc: '', body: "Build Failure ${RUN_DISPLAY_URL}", cc: '', from: 'kiranjatla9@gmail.com', replyTo: '', subject: 'Build failed', to: 'kiranjatla9@gmail.com'
         }
